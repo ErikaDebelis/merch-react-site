cfccabddee8cd183cbe43fbaf7a7f65917c26239
Erika Debelis <erika.debelis@gmail.com>
2021-09-08 15:25:13 -0700

    add more logic to NewDrinkForm to handle click event
    Co-Authored-By: Gurshan Kaile <gurshankaile206@gmail.com>

--- src/components/NewDrinkForm.js
+++ src/components/NewDrinkForm.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
-function NewDrinkForm(props) {
+function NewDrinkForm() {
+  function handleNewDrinkFormSubmission(event) {
+    event.preventDefault();
+  }
   return (
     <React.Fragment>
-
+      <form onSubmit={handleNewDrinkFormSubmission}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name" />
+      <input
+        type="text"
+        name="description"
+        placeholder="Description" />
+      <input
+        type="text"
+        name="quantity"
+        placeholder="Quantity" />
+      </form>
     </React.Fragment>
   );
 }
