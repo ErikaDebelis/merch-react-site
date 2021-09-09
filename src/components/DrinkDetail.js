751f272193560a412102fb1582e122ea917ab5da
Erika Debelis <erika.debelis@gmail.com>
2021-09-09 16:59:38 -0700

    add more styling to form in drinkdetail + buttons

--- src/components/DrinkDetail.js
+++ src/components/DrinkDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import Drink from './Drink';
+import Button from 'react-bootstrap/Button';
 
 function DrinkDetail(props) {
   const { drink, onClickingDelete } = props;
@@ -8,11 +9,11 @@ function DrinkDetail(props) {
   return(
     <React.Fragment>
       <h1>Drink Detail</h1>
-      <h4>{drink.name}</h4>
-      <h4>{drink.description}</h4>
-      <h4>{drink.quantity}</h4>
-      <button onClick = {props.onClickingEdit}>Update Drink</button>
-      <button onClick = {() => props.onClickingDelete(drink.id) }>Remove Drink</button>
+      <h4>Drink Name: {drink.name}</h4>
+      <h4>Drink Description: {drink.description}</h4>
+      <h4>Quantity: {drink.quantity}</h4>
+      <Button variant="outline-warning" size="sm" onClick = {props.onClickingEdit}>Update Drink</Button>
+      <Button variant="outline-danger" size="sm" onClick = {() => props.onClickingDelete(drink.id) }>Remove Drink</Button>
       <hr/>
     </React.Fragment>
   );
